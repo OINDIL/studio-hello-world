9eafdcfda8b7fb213ab78cc3178c780ba965ed9f
Oindil <oindilgolder303@gmail.com>
2025-12-04 09:02:26 +0530

    feat: add eventType to schemaTypes

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,4 @@
 import { navigationLinkType } from "./navigation/navigation-type";
+import { eventType } from "./events/event-type";
 
-export const schemaTypes = [navigationLinkType]
+export const schemaTypes = [navigationLinkType, eventType]
